ee9d4d87e06702161eff23bd3beac198b18d3859
Darya Sushkova <sushkova.darya8@yandex.ru>
2025-02-10 17:41:36 +0300

    Adde files

---
+++ src/main/java/com/example/athleteservice/AthleteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.athleteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthleteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthleteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/athleteservice/AthleteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.athleteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthleteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
